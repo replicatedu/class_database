7377a138bfc7b3065054c7f17984a106853088fc
alex <hortinstein@gmail.com>
2019-04-03 22:12:57 -0400

    added some example code

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@ use std::fs::OpenOptions;
 use std::io;
 
 use std::process::{Command};
+
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
+
 use hex;
 use class_crypto::{ClassCrypto};
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -34,14 +39,22 @@ fn command_wrapper(test_command: &str, command_directory: &str) -> Command {
 //rsa key generation
 //ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 
-pub fn get_rsa_key(){
+pub fn gen_rsa_keys(){
     let command = "rm -rf ./deploy_key* && \
                    ssh-keygen -f ./deploy_key -N '' -t rsa && \
-                   echo \"pase the following into deploy keys\" && \
+                   echo \"paste the following into deploy keys\" && \
                    cat deploy_key.pub &&
                    ssh-add -y -K ./deploy_key";
     let mut c = command_wrapper(&command, ".");
     println!("{}",String::from_utf8_lossy(&c.output().unwrap().stdout));
+    let command = "rm -rf ./deploy_key* && \
+                   ssh-keygen -f ./deploy_key -N '' -t rsa && \
+                   echo \"paste the following into deploy keys\" && \
+                   cat deploy_key.pub &&
+                   ssh-add -y -K ./deploy_key";
+    let mut c = command_wrapper(&command, ".");
+    println!("{}",String::from_utf8_lossy(&c.output().unwrap().stdout));
+
 }
 
 
@@ -113,6 +126,12 @@ impl ClassDatabase {
         let mut c = command_wrapper(&command, "/tmp/test_database");
         println!("{}",String::from_utf8_lossy(&c.output().unwrap().stdout));
     }
+    pub fn create_repo(&self){
+        let mut command: String = "curl -u 'hortinstein' https://api.github.com/user/repos -d '{\"name\":\"REPOs\"}'".to_owned();
+        
+        let mut c = command_wrapper(&command, "/tmp/");
+        println!("{}",String::from_utf8_lossy(&c.output().unwrap().stdout));
+    }
 }
 pub fn time_since_epoch() -> String {
     let start = SystemTime::now();
@@ -127,14 +146,14 @@ mod tests {
     #[test]
     fn gen_test_keypair() {
 
-        ClassDatabase::turn_off_host_checks();
-        get_rsa_key();
-        let db = ClassDatabase::new("git@github.com:replicatedu/test_database.git");
+        // ClassDatabase::turn_off_host_checks();
+        // get_rsa_key();
+        // let db = ClassDatabase::new("git@github.com:replicatedu/test_database.git");
         
-        db.pull_class_database();
-        let mut filename:String = "test".to_owned();
-        filename+= &time_since_epoch();
-        db.add_file(&filename);
-
+        // db.pull_class_database();
+        // let mut filename:String = "test".to_owned();
+        // filename+= &time_since_epoch();
+        // db.add_file(&filename);
+        ClassDatabase::create_repo();
     }
 }
